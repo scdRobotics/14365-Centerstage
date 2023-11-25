899aeb2f79ea80fb74341be78a8902adc93ee5bd
SCD Robotics <43300604+scdRobotics@users.noreply.github.com>
2023-11-25 10:35:44 -0500

    Delivery commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Delivery.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Delivery extends Subsystem{
+
+    public Delivery(Telemetry telemetry, HardwareMap hardwareMap, ElapsedTime timer) {
+        super(telemetry, hardwareMap, timer);
+    }
+}
