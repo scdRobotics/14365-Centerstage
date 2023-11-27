4ee451c896a1806775bbd1f6029ddc50683e7de6
SCD Robotics <43300604+scdRobotics@users.noreply.github.com>
2023-11-27 16:26:58 -0500

    Delivery commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
@@ -1,13 +1,4 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-public class Odometry extends Subsystem{
-
-    public Odometry(Telemetry telemetry, HardwareMap hardwareMap, ElapsedTime timer) {
-        super(telemetry, hardwareMap, timer);
-    }
+public class Odometry {
 }
