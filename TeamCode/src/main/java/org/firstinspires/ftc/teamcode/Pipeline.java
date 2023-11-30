5c2533eb672bbe0c944db99dc7c4d06bb0a81d81
SCD Robotics <scdrobotics@gmail.com>
2023-11-30 17:25:09 -0500

    Vision subsystem update
    Created a pipeline to hold the method for vision, I presume

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pipeline.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class Pipeline extends OpenCvPipeline {
+    public void onViewportTapped(){
+        
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -4,9 +4,24 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 public class Vision extends Subsystem{
     public Vision(Telemetry telemetry, HardwareMap hardwareMap, ElapsedTime timer) {
+        public OpenCvCamera webcam1;
+
+        public void activateYellowPieplineGamera1(){
+            //webcam1.setPipeline
+
+             public void onOpened(){
+                 webcam1.startStreaming(1280,720);
+                 telemetry.addData("Camera Opened! ", "");
+                 telemetry.update();
+            }
+
+        }
+
         super(telemetry, hardwareMap, timer);
     }
 }
